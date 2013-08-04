3bdfe00b22549f532d19873dc4ccd5d6919b4a87
beepscore <support@beepscore.com>
2013-08-04 13:15:23 -0700

    Fix warning "Application Windows Are Expected To Have A Root View Controller At The End Of Application Launch".
    Add MainStoryboard and MyViewController.

---
+++ UICKeyChainStore/MyViewController.h
@@ -0,0 +1,13 @@
+//
+//  MyViewController.h
+//  UICKeyChainStore
+//
+//  Created by Steve Baker on 8/4/13.
+//  Copyright (c) 2013 Beepscore LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MyViewController : UIViewController
+
+@end
---
+++ UICKeyChainStore/MyViewController.m
@@ -0,0 +1,29 @@
+//
+//  MyViewController.m
+//  UICKeyChainStore
+//
+//  Created by Steve Baker on 8/4/13.
+//  Copyright (c) 2013 Beepscore LLC. All rights reserved.
+//
+
+#import "MyViewController.h"
+
+@interface MyViewController ()
+
+@end
+
+@implementation MyViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
