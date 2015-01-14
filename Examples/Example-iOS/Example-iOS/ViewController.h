ec60375b85c38c8ddcf03a5e5f3c0e51336bb178
kishikawa katsumi <kishikawakatsumi@mac.com>
2015-01-15 04:12:45 +0900

    Add example project

---
+++ Examples/Example-iOS/Example-iOS/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Example-iOS
+//
+//  Created by kishikawa katsumi on 2015/01/15.
+//  Copyright (c) 2015 kishikawa katsumi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ Examples/Example-iOS/Example-iOS/ViewController.m
@@ -0,0 +1,26 @@
+//
+//  ViewController.m
+//  Example-iOS
+//
+//  Created by kishikawa katsumi on 2015/01/15.
+//  Copyright (c) 2015 kishikawa katsumi. All rights reserved.
+//
+
+#import "ViewController.h"
+#import <UICKeyChainStore/UICKeyChainStore.h>
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+}
+
+@end
