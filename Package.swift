1a834ff7c4f705047d6a7698b63c043a6179b9e8
Kishikawa Katsumi <kishikawakatsumI@mac.com>
2020-04-29 10:02:43 +0900

    Package.swift

--- Package.swift
+++ Package.swift
@@ -1,25 +1,16 @@
 // swift-tools-version:5.0
+
 import PackageDescription
 
 let package = Package(
     name: "UICKeyChainStore",
+    platforms: [
+        .macOS(.v10_10), .iOS(.v8), .tvOS(.v9), .watchOS(.v2)
+    ],
     products: [
         .library(name: "UICKeyChainStore", targets: ["UICKeyChainStore"])
     ],
     targets: [
-        .target(
-            name: "UICKeyChainStore",
-            path: "Lib/UICKeyChainStore",
-            publicHeadersPath: ""
-        ),
-        .target(
-            name: "TestHost",
-            path: "Lib/TestHost"
-        ),
-        .testTarget(
-            name: "UICKeyChainStoreTests",
-            dependencies: ["UICKeyChainStore", "TestHost"],
-            path: "Lib/UICKeyChainStoreTests"
-        )
+        .target(name: "UICKeyChainStore", path: "Lib/UICKeyChainStore")
     ]
 )
